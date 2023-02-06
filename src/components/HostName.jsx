2a88edfffe8dfbb1e9b1826e31d22b0f82082d53
Antoine <supMBH@gmail.com>
2023-02-06 12:40:35 +0100

    commentaire

--- src/components/HostName.jsx
+++ src/components/HostName.jsx
@@ -1,7 +1,8 @@
 
 import "../assets/styles/HostName.css";
 
-
+//on retourne ici le nom et la photo de l'hôte en utilisant split pour séparer nom et prénom avec espace
+//on retourne une div pour nom et prénom sur 2 paragraphes et une div avec l'image que l'on sort de la propriété picture de host
 function HostName({ host }) {
   const [name, lastname] = host.name.split(" ");
 
