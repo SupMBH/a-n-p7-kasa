0ff7ffedb86dabf158aeadc27477879d0cf0475f
Antoine <supMBH@gmail.com>
2023-02-06 07:20:16 +0100

    commentaire

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,6 +4,8 @@ import "../../assets/styles/About.css";
 import AboutBanner from "../../components/AboutBanner";
 import Collapse from "../../components/Collapse";
 
+// Affiche l'interface About composé du composant enfant AboutBanner qui reçoit la propriété picture avec la valeur imgBanner, et le composant collapse
+// On map le tableau aboutData pour que le composant enfant renvoie un collapse avec son titre et son contenu pour chaque itération data du tableau
 function About() {
 return (
 <div className="about">
@@ -18,4 +20,3 @@ return (
 }
 
 export default About;
-//R
